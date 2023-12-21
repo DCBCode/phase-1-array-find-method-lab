ef31174933f939e4919309ebd3cec30055eb1878
Drew Benson <Drewbenson16@gmail.com>
2023-12-21 11:08:25 -0700

    go broncos

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(array) {
+    const win = array.find((object) => object.result === "W");
+    if (win) {
+      return win.year;
+    } else {
+      return undefined;
+    }
+}
